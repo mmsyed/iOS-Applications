e9e920aecfd41587d5dd7c64ba39944597abd646
mmsyed <mmsyed@syr.edu>
2019-06-07 23:44:19 -0400

    to-do added, not complete

---
+++ To-Do List/To-Do List/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  To-Do List
+//
+//  Created by Mo Syed on 6/7/19.
+//  Copyright © 2019 Mo Syed. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ To-Do List/To-Do List/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  To-Do List
+//
+//  Created by Mo Syed on 6/7/19.
+//  Copyright © 2019 Mo Syed. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
