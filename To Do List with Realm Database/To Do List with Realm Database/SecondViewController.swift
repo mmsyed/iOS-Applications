3ebbc0cfd59073a400d084b86f4a2b94559897e0
mmsyed <mmsyed@syr.edu>
2019-06-09 12:48:11 -0400

    completed project

--- To Do List with Realm Database/To Do List with Realm Database/SecondViewController.swift
+++ To Do List with Realm Database/To Do List with Realm Database/SecondViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var text_view: UITextField!
+    
+    @IBAction func add_b(_ sender: Any) {
+        let sendtask = FirstViewController()
+        if (!(text_view.text == "")) {
+            sendtask.add_task(task1: text_view.text!)
+    }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.text_view.delegate = self
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        text_view.resignFirstResponder()
+        let send = FirstViewController()
+        if (!(text_view.text == "")) {
+            send.add_task(task1: text_view.text!)
+        }
+
+        return true
+    }
 
 }
 
