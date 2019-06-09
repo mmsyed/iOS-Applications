d4b5fff67b1224fb533b4a1c55e4c302b852f2e3
mmsyed <mmsyed@syr.edu>
2019-06-08 22:43:17 -0400

    update

---
+++ To Do List with Realm Database/To Do List with Realm Database/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  To Do List with Realm Database
+//
+//  Created by Mo Syed on 6/8/19.
+//  Copyright © 2019 Mo Syed. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ To Do List with Realm Database/To Do List with Realm Database/tasks.swift
@@ -0,0 +1,9 @@
+//
+//  tasks.swift
+//  To Do List with Realm Database
+//
+//  Created by Mo Syed on 6/8/19.
+//  Copyright © 2019 Mo Syed. All rights reserved.
+//
+
+import Foundation
