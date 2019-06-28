fb530a2cf7e01911366bf185fa18db452bb86170
mmsyed <mmsyed@syr.edu>
2019-06-27 23:23:28 -0400

    70% completion

--- Instagram Clone/Instagram Clone/ViewController.swift
+++ Instagram Clone/Instagram Clone/ViewController.swift
@@ -45,8 +45,6 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
         self.dismiss(animated: true, completion: nil)
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
