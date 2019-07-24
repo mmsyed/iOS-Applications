253a2b4c3ac42e33e91c170d0a84a3d42228a3b8
mmsyed <mmsyed@syr.edu>
2019-07-23 23:32:39 -0400

    upload

---
+++ Energize/Energize/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  Energize
+//
+//  Created by Mo Syed on 7/23/19.
+//  Copyright © 2019 Mo Syed. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Energize/Energize/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Energize
+//
+//  Created by Mo Syed on 7/23/19.
+//  Copyright © 2019 Mo Syed. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
