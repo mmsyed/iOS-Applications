498a0e60f327ff1c0c613114b2e70eafd8edd1ba
mmsyed <mmsyed@syr.edu>
2019-07-30 23:53:36 -0400

    clean

--- Energize/Energize/AppDelegate.swift
+++ Energize/Energize/AppDelegate.swift
@@ -13,8 +13,6 @@ import Stripe
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         STPPaymentConfiguration.shared().publishableKey = "pk_test_U5DWkyr5HDmReZSr4Pm1US3200Vv3uFsVu"
         // do any other necessary launch configuration
